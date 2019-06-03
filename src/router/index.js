4a4b9b9e280ced28b9c80cde77e1acbc4a72df91
CHARLOTTE <15668618532@163.com>
2019-06-03 23:18:37 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @ -- 自动找到 src 文件夹
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/',
+      component: Login
+
     }
   ]
 })
