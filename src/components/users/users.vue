bef13dd463c257d42a2446e7e99e55e6a4cf1ae3
CHARLOTTE <15668618532@163.com>
2019-06-04 16:52:40 +0800

    配置main界面的路由

---
+++ src/components/users/users.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>user 测试</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // @ -- 自动找到 src 文件夹
 import Login from '@/components/login/login.vue'
 import Home from '@/components/home/home.vue'
+import Users from '@/components/users/users.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
   }, {
     name: 'home',
     path: '/',
-    component: Home
+    component: Home,
+    children: [{
+      name: 'users',
+      path: '/users',
+      component: Users
+    }]
   }]
 })
