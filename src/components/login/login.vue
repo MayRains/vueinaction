2e099b30c5d56f4eda3ddbaed63e209d8cb3a625
CHARLOTTE <15668618532@163.com>
2019-06-04 14:35:58 +0800

    登录成功保存token

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -34,23 +34,37 @@ export default {
     }
   },
   methods: {
-    handleLogin () {
-      this.$http.post('login', this.formLabelAlign).then(res => {
-        // console.log(res)
-        // 登陆成功-- 跳转界面 提示登陆成功 || 不成功 --提示原因 msg
-        const {
-          // eslint-disable-next-line no-unused-vars
-          data,
-          // eslint-disable-next-line no-unused-vars
-          meta: { msg, status }
-        } = res.data
-        if (status === 200) {
-          // this.$router.push({ name: 'home' })
-          this.$message.success(msg)
-        } else {
-          this.$message.error(msg)
-        }
-      })
+    async handleLogin () {
+      // this.$http.post('login', this.formLabelAlign).then(res => {
+      //   // console.log(res)
+      //   // 登陆成功-- 跳转界面 提示登陆成功 || 不成功 --提示原因 msg
+      //   const {
+      //     // eslint-disable-next-line no-unused-vars
+      //     data,
+      //     meta: { msg, status }
+      //   } = res.data
+      //   if (status === 200) {
+      //     // 跳转 home
+      //     this.$router.push({ name: 'home' })
+      //     this.$message.success(msg)
+      //   } else {
+      //     this.$message.error(msg)
+      //   }
+      // })
+      // 让异步代码看起来像同步代码 ES7 - async+await
+      const res = await this.$http.post('login', this.formLabelAlign)
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data
+      if (status === 200) {
+        // 保存 token
+        localStorage.setItem('token', data.token)
+        this.$router.push({ name: 'home' })
+        this.$message.success(msg)
+      } else {
+        this.$message.error(msg)
+      }
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // @ -- 自动找到 src 文件夹
 import Login from '@/components/login/login.vue'
+import Home from '@/components/home/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      name: 'login',
-      path: '/',
-      component: Login
-
-    }
-  ]
+  routes: [{
+    path: '/',
+    redirect: '/login'
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }, {
+    name: 'home',
+    path: '/',
+    component: Home
+  }]
 })
